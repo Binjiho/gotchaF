e13455b61f62f287fcf14fc650e48626218d242e
FRANK <jiyu1948@gmail.com>
2024-02-28 20:43:59 +0900

    header 제거

--- frontend/components/layout/common.js
+++ frontend/components/layout/common.js
@@ -3,9 +3,9 @@ import Head from "next/head";
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
+      {/*<Head>*/}
+      {/*  <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />*/}
+      {/*</Head>*/}
       <div className={`min-h-full max-w-layout mx-auto my-0 bg-white`}>
         <div className={`min-h-[100vh]`}>{children}</div>
       </div>
