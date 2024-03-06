c83a8aa78efbff46574684e3b6fd95ee1f538c09
limjaehee <rhak39@gmail.com>
2024-03-06 11:22:02 +0900

    헤더 설정

--- frontend/components/layout/common.js
+++ frontend/components/layout/common.js
@@ -1,11 +1,21 @@
 import Head from "next/head";
 
+export const metadata = {
+  title: "HotForum : 당신의 이야기를 실현시키는 곳",
+  description: "프론트엔드 개발자가 되고 싶은 취준생의 블로그",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function Layout({ children }) {
   return (
     <>
-      {/*<Head>*/}
-      {/*  <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />*/}
-      {/*</Head>*/}
+      <Head>
+        <title>같차</title>
+        <meta property="og:title" content="Santiago" key="title" />
+        <link rel="icon" href="/favicon/gotcha.ico" />
+      </Head>
       <div className={`min-h-full max-w-layout mx-auto my-0 bg-white`}>
         <div className={`min-h-[100vh]`}>{children}</div>
       </div>
